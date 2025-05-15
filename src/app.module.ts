bb3d6f99a776b634b329a24f4cef653774e77147
h$.weird <dmanh20112000@gmail.com>
2025-05-15 11:17:32 +0700

    add static file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,21 @@ import { DatabaseModule } from '@/infra/database/database.module';
 import { WinstonModule } from 'nest-winston';
 import { winstonConfig } from '@/infra/config/logger.config';
 import { ContainerModules } from './modules';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'node:path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: path.join(process.cwd(), 'storages'),
+      useGlobalPrefix: false,
+      serveRoot: '/assets',
+      exclude: ['/api/{*test}'],
+      serveStaticOptions: {
+        fallthrough: false,
+        cacheControl: true,
+      },
+    }),
     WinstonModule.forRoot(winstonConfig),
     SecretModule,
     DatabaseModule,
